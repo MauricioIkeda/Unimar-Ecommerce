12f473b8b0abfdba10ca667d84a0f41d548f75c5
Mauricio Ikeda <mauricioikeda17@gmail.com>
2025-04-16 20:06:48 -0300

    .

--- store/models.py
+++ store/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Categoria(models.Model):
+    nome = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.nome
+
+class Produto(models.Model):
+    nome = models.CharField(max_length=50)
+    preco = models.DecimalField(default=0, decimal_places=2, max_digits=6)
+    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE, default=1)
+    descricao = models.CharField(max_length=255, default='', blank=True, null=True)
+    imagem = models.ImageField(upload_to='uploads/produtos/')
+    vendedor = models.ForeignKey(User, on_delete=models.CASCADE)
