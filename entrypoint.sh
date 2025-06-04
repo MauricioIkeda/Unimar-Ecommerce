d85c2e6bcb049b39e5c275a6e628c6fec7ac45ce
Vinicius-Brandi <vviniciusbrandi@gmail.com>
2025-06-04 20:22:25 -0300

    Create entrypoint.sh

---
+++ entrypoint.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+export PYTHONPATH="/app:$PYTHONPATH"
+
+# Aplica migrações do banco de dados
+echo "Applying database migrations..."
+python manage.py migrate --noinput || { echo "Database migrations failed!" && exit 1; }
+
+# Inicia o servidor Gunicorn
+echo "Starting Gunicorn server..."
+
+exec gunicorn Core.wsgi:application --bind 0.0.0.0:8000 --workers 3
